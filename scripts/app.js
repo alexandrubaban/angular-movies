5173942024fef6e1a025179cb144b14bcad3f7f6
Johnny Oshika <johnnyoshika@gmail.com>
2013-12-23 22:47:20 -0800

    Angular JS set up.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="angular.js" />
+
+angular.module('app', [])
+
+.controller('AppCtrl', function ($scope) {
+
+    $scope.name = 'Alexandru';
+    $scope.cities = [
+        'Vancouver',
+        'Burnaby',
+        'Coquitlam'
+    ];
+});
