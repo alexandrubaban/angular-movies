6ff0ae6553fde132a550fcf02defc2847b48af22
Alexandru Baban <alexbaban@gmail.com>
2014-01-05 02:29:49 -0800

    Get and show list with movies.

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,50 @@
 
 angular.module('app', [])
 
-.controller('AppCtrl', function ($scope) {
+.controller('AppCtrl', function ($scope, moviesData) {
 
-    $scope.name = 'Alexandru';
-    $scope.cities = [
-        'Vancouver',
-        'Burnaby',
-        'Coquitlam'
-    ];
+    var page = 1;
+    var pageLimit = 5;
+    
+    $scope.movies = [];
+
+    var loadData = function () {
+        moviesData.getMovies(page, pageLimit).then(function (response) {  
+            $scope.movies = response.data.movies;                                      
+        });
+    }    
+    
+    $scope.hasNext = function () {
+        return ($scope.movies.length === pageLimit);
+    }
+    
+    $scope.showNext = function () {
+        page += 1;
+        loadData();
+    }
+
+    $scope.hasPrev = function () {
+        return (page - 1);
+    }
+
+    $scope.showPrev = function () {
+       page -= 1;
+        loadData();
+    }
+
+    loadData();    
+
+})
+
+.factory('moviesData', function ($http) {
+    return {
+        getMovies: function (page, pageLimit) {
+            var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json";
+            var apikey = "sxu85vhd8898raqdwe7kx78y";
+        	
+        	var url = baseUrl + '?apikey=' + apikey + '&page=' + page + '&page_limit=' + pageLimit + '&callback=JSON_CALLBACK';
+
+            return $http.jsonp(url);
+        }
+    }
 });
